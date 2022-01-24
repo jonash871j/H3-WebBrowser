8ad3e6dfc0f49d3bbcd26f8d6be286e8c06ee568
Jonas Hansen <jonas.hansen@visma.com>
2022-01-24 11:49:48 +0100

    Add project files.

---
+++ WebBrowser/Program.cs
@@ -0,0 +1,98 @@
+﻿using HtmlAgilityPack;
+using WebBrowserLib;
+
+namespace WebBrowser
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            string url = "https://www.york.ac.uk/teaching/cws/wws/webpage1.html";
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("--- Web browser\n");
+                Console.WriteLine("a. Show raw html");
+                Console.WriteLine("b. Show html in simplified format\n");
+                Console.Write("Choice: ");
+                char choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 'a':
+                        string rawHtml = await WebBrowserHandler.GetRawHtml(url);
+                        Console.WriteLine(rawHtml);
+                        break;
+                    case 'b':
+                        HtmlDocument htmlDocument = await WebBrowserHandler.GetHtmlDocument(url);
+                        PrintHtmlInSimplifiedFormat(htmlDocument.DocumentNode.ChildNodes);
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid choice");
+                        break;
+                }
+                
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        public static void PrintHtmlInSimplifiedFormat(HtmlNodeCollection childNodes)
+        {
+            // Goes though each html node and prints selected tags
+            foreach (HtmlNode node in childNodes)
+            {
+                if (node.HasChildNodes)
+                {
+                    PrintHtmlInSimplifiedFormat(node.ChildNodes);
+                }
+                PrintTagContent(node.OriginalName.ToLower(), node.InnerText);
+            }
+        }
+
+        public static void PrintTagContent(string tag, string text)
+        {
+            switch (tag)
+            {
+                case "h1":
+                    WriteColorLine(ConsoleColor.Red, text);
+                    break;
+                case "h2":
+                    WriteColorLine(ConsoleColor.Yellow, text);
+                    break;
+                case "h3":
+                    WriteColorLine(ConsoleColor.Green, text);
+                    break;
+                case "h4":
+                    WriteColorLine(ConsoleColor.Cyan, text);
+                    break;
+                case "h5":
+                    WriteColorLine(ConsoleColor.Blue, text);
+                    break;
+                case "h6":
+                    WriteColorLine(ConsoleColor.Magenta, text);
+                    break;
+                case "p":
+                    WriteColorLine(ConsoleColor.White, text);
+                    break;
+                case "li":
+                    WriteColorLine(ConsoleColor.White, $"- {text}");
+                    break;
+                case "a":
+                    WriteColorLine(ConsoleColor.DarkBlue, text);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void WriteColorLine(ConsoleColor color, string text)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ WebBrowserLib/WebBrowserHandler.cs
@@ -0,0 +1,32 @@
+﻿using HtmlAgilityPack;
+
+namespace WebBrowserLib
+{
+    public class WebBrowserHandler
+    {
+        readonly static HttpClient client = new();
+
+        public static async Task<string> GetRawHtml(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public static async Task<HtmlDocument> GetHtmlDocument(string url)
+        {
+            string html = await GetRawHtml(url);
+
+            HtmlDocument htmlDocument = new();
+            htmlDocument.LoadHtml(html);
+            return htmlDocument;
+        }
+    }
+}
